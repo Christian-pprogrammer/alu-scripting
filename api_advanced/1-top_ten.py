6b664e3ca5e7eecf0c90162858034c5604652a86
Christian-pprogrammer <mpanoc6@gmail.com>
2025-11-14 15:02:07 +0200

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,41 +1,18 @@
 #!/usr/bin/python3
-"""Fetches and prints the titles of the first 10 hot posts for a subreddit."""
+"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a subreddit."""
-    if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-        return
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus403)"
-    }
-    params = {"limit": 10}
-
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-
-        if response.status_code != 200:
-            print(None)
-            return
-
-        data = response.json()
-        children = data.get("data", {}).get("children", [])
-
-        if len(children) == 0:
-            print(None)
-            return
-
-        for child in children:
-            print(child.get("data", {}).get("title"))
-
-    except Exception:
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
         print(None)
