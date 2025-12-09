90c11ddcb013f3970a898e1e9e8e10650ee57aa0
Christian-pprogrammer <mpanoc6@gmail.com>
2025-12-09 16:17:28 +0200

    pleaseee help me God

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,17 +4,26 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    """Prints titles of first 10 hot posts of a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'CustomUserAgent/1.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Only invalid if 404
+    if response.status_code == 404:
+        print("None")
+        return
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
         data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
+        posts = data.get("data", {}).get("children", [])
+
+        if not posts:
+            print("None")
+            return
+
+        for post in posts[:10]:
+            print(post["data"]["title"])
     except Exception:
-        print(None)
+        print("None")
